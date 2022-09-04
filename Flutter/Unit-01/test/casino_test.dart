01751ca1c4c7a60d303da851e380613e48531456
Anton Bevzuk <bevzuk@gmail.com>
2022-09-04 10:51:10 +0300

    Заготовка теста для Dart

--- Flutter/Unit-01/test/casino_test.dart
+++ Flutter/Unit-01/test/casino_test.dart
@@ -1,5 +1,9 @@
-import 'package:casino/chip.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  group('Test 1', () {
+    test('equals 1', () {
+      expect(1, 1);
+    });
+  });
 }
