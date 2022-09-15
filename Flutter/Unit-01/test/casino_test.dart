66576283ca3dbc8a584c7725dcdfaad20611cca1
Anton Bevzuk <bevzuk@gmail.com>
2022-09-15 13:55:19 +0300

    Почистил тест

--- Flutter/Unit-01/test/casino_test.dart
+++ Flutter/Unit-01/test/casino_test.dart
@@ -4,8 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('Test 1', () {
     test('equals 1', () {
-      Chip chip = Chip(100);
-      expect(chip, Chip(100));
+      expect(1, 1);
     });
   });
 }
