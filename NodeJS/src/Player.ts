4616501397cf526a9121a3ca676bbd835444a929
Anton Bevzuk <bevzuk@gmail.com>
2022-09-15 15:23:27 +0300

    Node.JS: игрок может войти и выйти из игры

--- NodeJS/src/Player.ts
+++ NodeJS/src/Player.ts
@@ -4,7 +4,17 @@ export class Player {
     _isInGame = false;
 
     enter(game: RollDiceGame) {
+        if (this._isInGame) {
+            throw new TypeError("Unable to enter another game");
+        }
         this._isInGame = true;
+    }
+
+    leaveGame() {
+        if (!this._isInGame) {
+            throw new TypeError("Unable to leave the game before entering");
+        }
+        this._isInGame = false;
     }
 
     isInGame(): boolean {
--- NodeJS/test/unit/player.spec.ts
+++ NodeJS/test/unit/player.spec.ts
@@ -19,4 +19,30 @@ describe('Когда игрок играет в кости', function() {
 
         expect(player.isInGame()).to.equal(true);
     });
+
+    it('он может выйти из игры', function() {
+        let player = new Player();
+        let game = new RollDiceGame();
+        player.enter(game);
+
+        player.leaveGame();
+
+        expect(player.isInGame()).to.equal(false);
+    });
+
+    it('он не может выйти из игры, если он в нее не входил', function() {
+        let player = new Player();
+        let game = new RollDiceGame();
+
+        expect(() => player.leaveGame()).to.throw(TypeError, "Unable to leave the game before entering");
+    });
+
+    it('он может играть только в одну игру одновременно', function() {
+        let player = new Player();
+        let game = new RollDiceGame();
+        let anotherGame = new RollDiceGame();
+        player.enter(game);
+
+        expect(() => player.enter(anotherGame)).to.throw(TypeError, "Unable to enter another game");
+    });
 });
