b50be1dda07042acd968d57a6757ae5f00dcc022
Anton Bevzuk <bevzuk@gmail.com>
2022-09-15 16:10:16 +0300

    Node.JS: удалил код из примера

--- NodeJS/src/greetings.ts
+++
@@ -1,9 +0,0 @@
-import { INamed } from "./INamed";
-
-
-export const simpleGreeting = () => 'hello';
-
-
-export const targetedGreeting = function(entity: INamed) {
-    return `Hello, ${entity.name()}!`;
-};
