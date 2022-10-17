2ff96f07a0c714e8a3f6fe7a6d9957cae7c38839
sh16ee <sh16ee@gmail.com>
2022-10-17 11:44:37 +0600

    Adik zver

--- Flutter/Unit-01/test/casino_test.dart
+++ Flutter/Unit-01/test/casino_test.dart
@@ -1,3 +1,6 @@
+import 'package:casino/chip.dart';
+import 'package:casino/player.dart';
+import 'package:casino/roll_dice_game.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -5,5 +8,25 @@ void main() {
     test('equals 1', () {
       expect(1, 1);
     });
+    test('less operator', () {
+      expect(Chip(2)>=Chip(1), true);
+    });
+    test('player join/leave game', () {
+      Player player = Player();
+      player.join(RollDiceGame());
+      expect(player.isInGame(), true);
+      player.leaveGame();
+      expect(player.isInGame(), false);
+    });
+    test('player buy/has', () {
+      Player player = Player();
+      player.buy(Chip(1));
+      expect(player.has(Chip(1)), true);
+    });
+    // test('player win/lose', () {
+    //   Player player = Player();
+    //   player.join(RollDiceGame());
+    //   expect(player.win(Chip(0)), true);
+    // });
   });
 }
