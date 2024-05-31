e1e11a5011ad52e22088705c7ec98f8b9a6a20bc
Oleg Blokhin <o.blokhin@vk.team>
2024-05-31 12:17:32 +0300

    python tests

--- Python/tests/test_chip.py
+++ Python/tests/test_chip.py
@@ -1,17 +1,18 @@
-from Python.chip import Chip
-from Python.bet import Bet
-from Python.player import Player
+from app.chip import Chip
+from app.bet import Bet
+from app.player import Player
 
 
 def test_save_chips_amount():
     assert Chip(1).amount == 1
+
 
 def test_save_bet_details():
     bet = Bet(Chip(1), 6)
     assert bet.chips.amount == 1
     assert bet.score == 6
 
+
 def test_new_player_is_not_in_game():
     player = Player()
-    assert player.is_in_game() == False
-
+    assert not player.is_in_game()
