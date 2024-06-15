c299c97c82d0253a6d2cab52773fbce6a36d68f8
Anton Bevzuk <bevzuk@gmail.com>
2024-06-15 17:37:48 +0300

    Player can't join two different games

--- Python/app/casino.py
+++ Python/app/casino.py
@@ -1,14 +1,33 @@
+from typing import List
+
+from app.Exceptions.invalid_operation_exception import InvalidOperationException
 from app.chips import Chips
 from app.game import Game
 from app.player import Player
 
 
 class Casino:
+    def __init__(self):
+        self._games: List[Game] = []
+
     def sell(self, player: Player, chips: Chips):
         player.deposit(chips)
 
     def get_open_game(self):
-        return Game()
+        game = Game()
+        self._games.append(game)
+        return game
 
     def get_players_of(self, game: Game):
         return game.get_players()
+
+    def add(self, player: Player, game: Game):
+        if self.player_already_in_game(player):
+            raise InvalidOperationException("Player should leave the game before joining another game")
+        game.add(player)
+
+    def player_already_in_game(self, player: Player):
+        for game in self._games:
+            if player in game.get_players():
+                return True
+        return False
--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -43,5 +43,18 @@ def test_game_cant_add_player_second_time():
 
     with pytest.raises(InvalidOperationException) as e:
         game.add(player)
-
+        
     assert str(e.value) == "Player can't be added second time"
+
+
+def test_two_games_can_not_add_same_player():
+    casino = Casino()
+    player = Player()
+    game = casino.get_open_game()
+    another_game = casino.get_open_game()
+    casino.add(player, game)
+
+    with pytest.raises(InvalidOperationException) as e:
+        casino.add(player, another_game)
+
+    assert str(e.value) == "Player should leave the game before joining another game"
