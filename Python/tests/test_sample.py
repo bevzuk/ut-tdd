7b1388ef2052a455a4b593ed11dd6081f7745092
Elena Gvozdeva <e.gvozdeva@yadro.com>
2024-06-19 11:00:44 +0300

    add init tests

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -3,3 +3,15 @@ from app import *
 
 def test_pass():
     assert Chip(1) == Chip(1)
+
+def test_buy():
+    chips = Chip(3)
+    player = Player()
+    player.buy(chips)
+    assert player._available_chips == chips
+
+def test_init_player():
+    player = Player()
+    assert player._available_chips == Chip(0)
+    assert player._current_game is None
+
