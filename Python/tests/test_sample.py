5de08da059d1ab093baa479cc26e8009880fa194
Yuliya Kamelina <y.kamelina@yadro.com>
2024-06-19 12:21:58 +0300

    update test namings AAA

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,48 +1,105 @@
 from app import *
+from app.Exceptions.invalid_operation_exception import InvalidOperationException
+import unittest
+
+class TestDice(unittest.TestCase):
+    def test_should_roll_less_than_6(self):
+        assert Dice.roll() <= 6
+
+    def test_should_roll_greater_than_1(self):
+        assert Dice.roll() >= 1
 
 
-def test_pass():
-    assert Chip(1) == Chip(1)
+class TestBet(unittest.TestCase):
+    def test_can_init(self):
+        bet = Bet(Chip(1), 1)
 
-def test_dice_le6():
-    assert Dice.roll() <= 6
+        assert bet.chips == Chip(1)
+        assert bet.score == 1
 
-def test_dice_ge1():
-    assert Dice.roll() >= 1
 
-def test_bet_init():
-    bet = Bet(Chip(1), 1)
-    assert bet.chips == Chip(1)
-    assert bet.score == 1
+class TestChip(unittest.TestCase):
+    def test_pass(self):
+        assert Chip(1) == Chip(1)
 
-def test_chip_ge():
-    chip1 = Chip(1)
-    chip2 = Chip(2)
-    assert chip2 >= chip1
+    @unittest.skip('Invalid code, should be fixed')
+    def test_cant_assign_negative(self):
+        with self.assertRaises(BaseException):
+            Chip(-1)
 
-def test_chip_le():
-    chip1 = Chip(1)
-    chip2 = Chip(2)
-    assert chip1 <= chip2
+    def test_greater_chips_are_greater(self):
+        chip1 = Chip(1)
+        chip2 = Chip(2)
 
-def test_chip_add():
-    chip1 = Chip(1)
-    chip2 = Chip(2)
-    chip_sum = chip1 + chip2
-    assert chip_sum._amount == 3
+        assert chip2 >= chip1
 
-def test_chip_sub():
-    chip1 = Chip(11)
-    chip2 = Chip(29)
-    chip_sub = chip2 - chip1
-    assert chip_sub._amount == 18
+    def test_smaller_chips_are_smaller(self):
+        chip1 = Chip(1)
+        chip2 = Chip(2)
 
-def test_chip_mul():
-    chip1 = Chip(9945)
-    chip_mul = chip1 * 198
-    assert chip_mul._amount == 1969110
+        assert chip1 <= chip2
 
-def test_player_init():
-    player = Player()
-    assert player._available_chips._amount == 0
+    def test_can_add_another_chip(self):
+        chip1 = Chip(1)
+        chip2 = Chip(2)
 
+        chip_sum = chip1 + chip2
+
+        assert chip_sum == Chip(3)
+
+    def test_can_sub_smaller_chip(self):
+        chip1 = Chip(11)
+        chip2 = Chip(29)
+
+        chip_sub = chip2 - chip1
+
+        assert chip_sub == Chip(18)
+
+    @unittest.skip('Invalid code, should be fixed')
+    def test_cant_sub_greater_chip(self):
+        chip1 = Chip(11)
+        chip2 = Chip(29)
+
+        with self.assertRaises(BaseException):
+            chip1 - chip2
+
+    def test_can_mul_value(self):
+        chip1 = Chip(9945)
+
+        chip_mul = chip1 * 198
+
+        assert chip_mul == Chip(1969110)
+
+
+class TestPlayer(unittest.TestCase):
+    @staticmethod
+    def setup_player_in_game():
+        game = RollDiceGame()
+        player = Player()
+        player.join(game)
+
+        return (player, game)
+
+    def test_can_init(self):
+        player = Player()
+
+        assert isinstance(player, Player)
+
+    def test_init_with_zero_chips(self):
+        assert Player().has(Chip(0))
+        assert Player().has(Chip(1)) is False
+
+    def test_can_join_to_game(self):
+        game = RollDiceGame()
+        player = Player()
+
+        try:
+            player.join(game)
+        except BaseException:
+            self.fail("player cannot join to game!")
+
+    def test_cannot_join_to_game_twice(self):
+        player, game = self.setup_player_in_game()
+
+        with self.assertRaises(InvalidOperationException):
+            player.join(game)
