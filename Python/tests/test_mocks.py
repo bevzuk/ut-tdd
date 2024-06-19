9d22d9ea4346667ce2f04c214299ced2d9beb611
Nikita Ignatovskiy <n.ignatovskiy@yadro.com>
2024-06-19 15:19:00 +0300

    Add shitty DSL.

--- Python/tests/test_mocks.py
+++ Python/tests/test_mocks.py
@@ -1,34 +1,35 @@
 from unittest.mock import MagicMock
 
-from pytest import fixture
-
 from app import Player, RollDiceGame, Chip, Bet, Dice
 
 
-@fixture
-def setup_game_with_defined_dice_value():
-    def _setup_game_with_defined_dice_value(is_win=True):
-        dice = Dice()
-        if is_win:
-            dice.roll = MagicMock(return_value=6)
-        else:
-            dice.roll = MagicMock(return_value=1)
+class GameDSL:
+    def __init__(self):
+        self.dice = Dice()
+        self.player = Player()
+        self.game = RollDiceGame(self.dice)
+        self.player.join(self.game)
+        self.player.buy(Chip(10))
 
-        test_game = RollDiceGame(dice)
-        test_player = Player()
-        test_player.join(test_game)
-        test_player.buy(Chip(10))
-        test_game.bet(test_player, Bet(Chip(10), 6))
-        test_game.play()
-        return test_player
-    return _setup_game_with_defined_dice_value
+    def set_win(self):
+        self.dice.roll = MagicMock(return_value=6)
+        return self
+
+    def set_loss(self):
+        self.dice.roll = MagicMock(return_value=1)
+        return self
+
+    def play_game(self):
+        self.game.bet(self.player, Bet(Chip(10), 6))
+        self.game.play()
+        return self.player
 
 
-class TestPlayer():
-    def test_should_win(self, setup_game_with_defined_dice_value):
-        player = setup_game_with_defined_dice_value(is_win=True)
+class TestPlayer:
+    def test_should_win(self):
+        player = GameDSL().set_win().play_game()
         assert player.has(Chip(60)) is True
 
-    def test_should_lose(self, setup_game_with_defined_dice_value):
-        player = setup_game_with_defined_dice_value(is_win=False)
+    def test_should_lose(self):
+        player = GameDSL().set_loss().play_game()
         assert player.has(Chip(0)) is True
