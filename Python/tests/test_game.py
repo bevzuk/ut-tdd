261797f6ea2abe345ebc2320ea4374555832fb8e
just_sparta <vi.kuznetsov@yadro.com>
2024-06-19 12:40:43 +0300

    add fixture

--- Python/tests/test_game.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-from Python.app import *
-from Python.app.Exceptions.invalid_operation_exception import InvalidOperationException
--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -3,11 +3,17 @@ import pytest
 from Python.app import *
 
 
-def test_can_join_game():
+@pytest.fixture
+def setup_game_with_one_player():
     game = RollDiceGame()
     lucky_player = Player()
-
     lucky_player.join(game)
+
+    return game, lucky_player
+
+
+def test_can_join_game(setup_game_with_one_player):
+    _, lucky_player = setup_game_with_one_player
 
     assert lucky_player.is_in_game(), "Player cannot join the game"
 
@@ -20,10 +26,8 @@ def test_can_buy_chips():
     assert lucky_player.has(chips)
 
 
-def test_can_bet():
-    game = RollDiceGame()
-    lucky_player = Player()
-    lucky_player.join(game)
+def test_can_bet(setup_game_with_one_player):
+    game, lucky_player = setup_game_with_one_player
     lucky_player.buy(Chip(4))
 
     try:
@@ -32,21 +36,17 @@ def test_can_bet():
         assert False, "Player cannot bet 2 chips"
 
 
-def test_can_leave_game():
-    game = RollDiceGame()
-    lucky_player = Player()
-    lucky_player.join(game)
+def test_can_leave_game(setup_game_with_one_player):
+    game, lucky_player = setup_game_with_one_player
 
     lucky_player.leave_game()
 
     assert lucky_player.is_in_game() is False, "Player cannot leave the game"
 
 
-def test_cannot_join_2games():
-    game = RollDiceGame()
+def test_cannot_join_2games(setup_game_with_one_player):
+    game, lucky_player = setup_game_with_one_player
     second_game = RollDiceGame()
-    lucky_player = Player()
-    lucky_player.join(game)
 
     with pytest.raises(BaseException):
         lucky_player.join(second_game)
