7c1ad5916fc65b1bf0511c4226a567235537fe9c
Kirill Yashin <k.yashin@yadro.com>
2024-06-19 11:36:44 +0300

    Added some new tests

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -26,6 +26,12 @@ class TestGame(unittest.TestCase):
     pass
 
 
+class TestDice(unittest.TestCase):
+    def test_is_rolling(self):
+        dice = Dice()
+        assert type(dice.roll()) == int
+
+
 class TestPlayer(unittest.TestCase):
     def test_cannot_join_twice_on_same_game(self):
         player = Player()
@@ -44,6 +50,42 @@ class TestPlayer(unittest.TestCase):
         with self.assertRaises(BaseException):
             player.join(game2)
 
+    def test_cannot_leave_game_twice(self):
+        player = Player()
+        game = RollDiceGame()
+        player.join(game)
+        player.leave_game()
 
-def test_pass():
-    assert Chip(1) == Chip(1)
+        with self.assertRaises(BaseException):
+            player.leave_game()
+
+    def test_can_join_game(self):
+        player = Player()
+        game = RollDiceGame()
+        assert player.join(game) is None
+
+    def test_can_buy_chips(self):
+        player = Player()
+        chips = Chip(10)
+        assert player.buy(chips) is None
+
+    def test_can_place_bet(self):
+        player = Player()
+        player.buy(Chip(10))
+        game = RollDiceGame()
+        bet = Bet(Chip(10), 10)
+        assert game.bet(player, bet) is None
+
+    def test_cannot_place_bet_if_not_enough_chips(self):
+        player = Player()
+        player.buy(Chip(10))
+        game = RollDiceGame()
+        bet = Bet(Chip(15), 1)
+        with self.assertRaises(BaseException):
+            game.bet(player, bet)
+
+    def test_can_leave_game(self):
+        player = Player()
+        game = RollDiceGame()
+        player.join(game)
+        assert player.leave_game() is None
