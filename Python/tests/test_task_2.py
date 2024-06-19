9b1ed1d452f6ebc00f8a9b0682a90e4f85b91a2f
Nikita Ignatovskiy <n.ignatovskiy@yadro.com>
2024-06-19 12:56:09 +0300

    Add fixture.

--- Python/tests/test_task_2.py
+++ Python/tests/test_task_2.py
@@ -1,31 +1,34 @@
-from app import *
-import unittest
+from pytest import fixture, mark
+from app import Player, RollDiceGame, Chip, Bet
 
 
-class TestPlayer(unittest.TestCase):
+@fixture(autouse=True)
+def setup_game_and_player_with_10_chips():
+    test_player = Player()
+    test_game = RollDiceGame()
+    test_player.join(test_game)
+    test_player.buy(Chip(10))
+    return test_player, test_game
 
-    def test_can_join_game(self):
-        test_player = Player()
-        test_player.join(RollDiceGame())
-        assert test_player.is_in_game() is True
 
-    def test_can_buy_chips(self):
-        test_player = Player()
-        test_player.buy(Chip(10))
-        assert test_player.has(Chip(10)) is True
+@mark.usefixtures("setup_game_and_player_with_10_chips")
+class TestPlayer():
+    def test_can_join_game(self, setup_game_and_player_with_10_chips):
+        player, _ = setup_game_and_player_with_10_chips
+        assert player.is_in_game() is True
 
-    def test_can_bet(self):
-        test_player = Player()
-        test_game = RollDiceGame()
-        test_player.join(test_game)
-        test_player.buy(Chip(10))
-        test_game.bet(test_player, Bet(Chip(10), 6))
-        test_game.play()
-        assert test_player.has(
-            Chip(60)) is True or test_player.has(Chip(0)) is True
+    def test_can_buy_chips(self, setup_game_and_player_with_10_chips):
+        player, _ = setup_game_and_player_with_10_chips
+        assert player.has(Chip(10)) is True
 
-    def test_can_leave_game(self):
-        test_player = Player()
-        test_player.join(RollDiceGame())
-        test_player.leave_game()
-        assert test_player.is_in_game() is False
+    def test_can_bet(self, setup_game_and_player_with_10_chips):
+        player, game = setup_game_and_player_with_10_chips
+        game.bet(player, Bet(Chip(10), 6))
+        game.play()
+        assert player.has(
+            Chip(60)) is True or player.has(Chip(0)) is True
+
+    def test_can_leave_game(self, setup_game_and_player_with_10_chips):
+        player, _ = setup_game_and_player_with_10_chips
+        player.leave_game()
+        assert player.is_in_game() is False
