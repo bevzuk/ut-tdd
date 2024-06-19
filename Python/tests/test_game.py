2b1941da344e140a25306169211f603b2abd8826
Ilya Naumov <i.n.1998@inbox.ru>
2024-06-19 11:40:33 +0300

    Exercise 2

---
+++ Python/tests/test_game.py
@@ -0,0 +1,28 @@
+import pytest
+
+from app import *
+from app.Exceptions.invalid_operation_exception import InvalidOperationException
+
+
+class TestsGame:
+    def test_player_can_bet_enough_chips(self):
+        game = RollDiceGame()
+        player = Player()
+        player.buy(Chip(10))
+        player.join(game)
+        bet = Bet(Chip(5), 3)
+
+        game.bet(player, bet)
+
+        assert (player.has(Chip(5)) is True)
+        assert (player.has(Chip(6)) is False)
+
+    def test_player_can_not_bet_not_enough_chips(self):
+        game = RollDiceGame()
+        player = Player()
+        player.buy(Chip(10))
+        player.join(game)
+        bet = Bet(Chip(100), 3)
+
+        with pytest.raises(InvalidOperationException):
+            game.bet(player, bet)
--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -1,40 +1,59 @@
 from app import *
 
-def test_buy():
-    player = Player()
-    player.buy(Chip(10))
-    assert (player.has(Chip(10)) is True)
 
-def test_win():
-    player = Player()
-    player.buy(Chip(10))
-    player.win(Chip(10))
-    assert (player.has(Chip(20)) is True)
+class TestsPlayer:
+    def test_can_join_game(self):
+        game = RollDiceGame()
+        player = Player()
 
-def test_take():
-    player = Player()
-    player.buy(Chip(10))
-    player.take(Chip(5))
-    assert (player.has(Chip(5)) is True)
+        player.join(game)
 
-def test_has_initial():
-    player = Player()
-    assert (player.has(Chip(0)) is True)
+        assert player.is_in_game()
 
-def test_has_equals_chips():
-    player = Player()
-    player.buy(Chip(10))
-    assert (player.has(Chip(10)) is True)
+    def test_can_leave_game(self):
+        game = RollDiceGame()
+        player = Player()
+        player.join(game)
 
-def test_has_less_chips():
-    player = Player()
-    player.buy(Chip(10))
-    assert (player.has(Chip(5)) is True)
+        player.leave_game()
 
-def test_has_not_enough_chips():
-    player = Player()
-    player.buy(Chip(5))
-    assert (player.has(Chip(10)) is False)
+        assert player.is_in_game() is False
 
+    def test_can_buy_chips(self):
+        player = Player()
 
-    
+        player.buy(Chip(10))
+
+        assert (player.has(Chip(10)) is True)
+        assert (player.has(Chip(11)) is False)
+
+    def test_win(self):
+        player = Player()
+        player.buy(Chip(10))
+        player.win(Chip(10))
+        assert (player.has(Chip(20)) is True)
+
+    def test_take(self):
+        player = Player()
+        player.buy(Chip(10))
+        player.take(Chip(5))
+        assert (player.has(Chip(5)) is True)
+
+    def test_has_initial(self):
+        player = Player()
+        assert (player.has(Chip(0)) is True)
+
+    def test_has_equals_chips(self):
+        player = Player()
+        player.buy(Chip(10))
+        assert (player.has(Chip(10)) is True)
+
+    def test_has_less_chips(self):
+        player = Player()
+        player.buy(Chip(10))
+        assert (player.has(Chip(5)) is True)
+
+    def test_has_not_enough_chips(self):
+        player = Player()
+        player.buy(Chip(5))
+        assert (player.has(Chip(10)) is False)
