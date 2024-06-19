f80964708ce6acaddf63430b8b2a0e0cb786da1d
just_sparta <vi.kuznetsov@yadro.com>
2024-06-19 15:48:40 +0300

    dsl

--- Python/tests/test_game_dsl.py
+++ Python/tests/test_game_dsl.py
@@ -9,3 +9,11 @@ def test_player_wins():
     state.game.play()
 
     assert state.check_player_chips(number=0, chip_count=4*6), "Game plays wrong"
+
+def test_player_lose():
+    state = Helper.create_game(fixed_dice_value=5).with_one_player().buy_chips_for_player(number=0, count=4)
+    state = state.player_place_a_bet(number=0, chip_count=4, score=4)
+
+    state.game.play()
+
+    assert state.check_player_chips(number=0, chip_count=0), "Game plays wrong"
