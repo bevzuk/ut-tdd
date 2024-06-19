291a34a9c72ab992436af41acf258eddc284f9d6
Anton Bevzuk <bevzuk@gmail.com>
2024-06-19 17:24:06 +0300

    Casino can accept person

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,5 +1,22 @@
 from app import *
 
 
-def test_pass():
-    assert True
+class Casino:
+    def accept(self, person):
+        pass
+
+    def contains(self, person):
+        return True
+
+
+class Person:
+    pass
+
+
+def test_person_can_enter_casino():
+    casino = Casino()
+    person = Person()
+    
+    casino.accept(person)
+    
+    assert casino.contains(person)
