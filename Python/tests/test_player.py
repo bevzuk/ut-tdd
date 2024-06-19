6e6e7c100023c231c9a12d947f4eb75d8498d029
Dmitry Pigasin <d.pigasin@yadro.com>
2024-06-19 12:41:29 +0300

    Add fuxtures

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -8,29 +8,52 @@ def player():
     return Player()
 
 
+@pytest.fixture(scope='function')
+def game():
+    return RollDiceGame()
+
+
+@pytest.fixture(scope='function')
+def player_in_game(player, game):
+    player.join(game)
+    return player, game
+
+
 def test_not_in_game(player):
     assert player.is_in_game() is False
 
 
-def test_can_join_game(player):
-    player.join(RollDiceGame())
+def test_can_join_game(player_in_game):
+    player, _ = player_in_game
     assert player.is_in_game() is True
 
 
 def test_can_buy_chips(player):
     player.buy(Chip(3))
+
     assert player.has(Chip(3))
 
 
-def test_can_bet(player):
-    game = RollDiceGame()
-    player.join(game)
+def test_can_bet(player_in_game):
+    player, game = player_in_game
     player.buy(Chip(3))
+
     game.bet(player, Bet(Chip(3), score=6))
+
     assert player.has(Chip(0))
 
-def test_can_leave_game(player):
-    game = RollDiceGame()
-    player.join(game)
+
+def test_cannot_bet(player_in_game):
+    player, game = player_in_game
+    player.buy(Chip(3))
+
+    with pytest.raises(InvalidOperationException):
+        game.bet(player, Bet(Chip(5), score=6))
+
+
+def test_can_leave_game(player_in_game):
+    player, _ = player_in_game
+
     player.leave_game()
+
     assert player.is_in_game() is False
