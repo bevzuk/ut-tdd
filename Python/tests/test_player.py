746b0eb6120942d08a8a94ccacae729887bb249a
Dmitry Kalyanov <kalyanowdima@mail.ru>
2024-06-19 12:43:00 +0300

    a few more test

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -1,33 +1,42 @@
 import pytest
 from app.roll_dice_game import RollDiceGame
+from app.Exceptions.invalid_operation_exception import InvalidOperationException
 from app.Exceptions.too_many_players_exception import TooManyPlayersException
 from app.player import Player
 from app.chip import Chip
 from app.bet import Bet
 
+@pytest.fixture
+def setup_player_with_5_chips_in_game():
+    player = Player()
+    game = RollDiceGame()
+    player.join(game)
+    chips = Chip(5)
+    player.buy(chips)
+
+    return player, game
 
 
-def test_new_player_has_no_chips():
+def test_has_no_chips_by_default():
     player = Player()
     assert player.has(Chip(0))
 
-def test_player_can_buy_chips():
-    player = Player()
-    chips = Chip(5)
-    player.buy(chips)
+def test_can_buy_chips(setup_player_with_5_chips_in_game):
+    player, _ = setup_player_with_5_chips_in_game
     assert player.has(Chip(5))
 
-def test_player_can_bet():
-    player = Player()
-    game = RollDiceGame()
-    game.add_player()
-    chips = Chip(5)
+def test_spend_chips_on_betting_in_game(setup_player_with_5_chips_in_game):
+    player, game = setup_player_with_5_chips_in_game
+    bet = Bet(Chip(2), 1)
 
-    player.buy(chips)
-    bet = Bet(chips, 1)
     game.bet(player, bet)
 
-    assert True
+    assert player.has(Chip(3))
+    assert not player.has(Chip(4))
 
+def test_when_not_enough_chips_cannot_bet(setup_player_with_5_chips_in_game):
+    player, game = setup_player_with_5_chips_in_game
+    bet = Bet(Chip(10), 3)
 
-
+    with pytest.raises(InvalidOperationException):
+        game.bet(player, bet)
