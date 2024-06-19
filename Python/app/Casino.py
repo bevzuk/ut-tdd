0dd1d5e3e14e7226b12e27e58d6b5862c8a0be59
Anton Bevzuk <bevzuk@gmail.com>
2024-06-19 17:33:39 +0300

    Persons count changes after casino accepts person

---
+++ Python/app/Casino.py
@@ -0,0 +1,13 @@
+class Casino:
+    
+    def __init__(self):
+        self._persons = []
+
+    def accept(self, person):
+        self._persons.append(person)
+
+    def contains(self, person):
+        return person in self._persons
+
+    def get_persons_count(self):
+        return len(self._persons)
--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,22 +1,26 @@
-from app import *
-
-
-class Casino:
-    def accept(self, person):
-        pass
-
-    def contains(self, person):
-        return True
-
-
-class Person:
-    pass
+from app.Casino import Casino
+from app.Person import Person
 
 
 def test_person_can_enter_casino():
     casino = Casino()
     person = Person()
-    
+
     casino.accept(person)
-    
+
     assert casino.contains(person)
+
+
+def test_casino_person_count_1_when_single_person_is_accepted():
+    casino = Casino()
+    person = Person()
+
+    casino.accept(person)
+
+    assert casino.get_persons_count() == 1
+
+
+def test_casino_has_no_persons_by_default():
+    casino = Casino()
+
+    assert casino.get_persons_count() == 0
