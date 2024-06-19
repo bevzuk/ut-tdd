37d195387047d31bdda1ad472fe3cf8a53bb58ab
Roman Vyunov <r.vyunov@yadro.com>
2024-06-19 11:11:25 +0300

    Chip tests

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,5 +1,61 @@
-from app import *
+from app import Chip
+from app.Exceptions.invalid_operation_exception import InvalidOperationException
+
+import pytest
 
 
-def test_pass():
+def test_eq():
     assert Chip(1) == Chip(1)
+
+
+def test_ge():
+    assert Chip(2) >= Chip(1)
+
+
+def test_ge_eq():
+    assert Chip(2) >= Chip(2)
+
+
+def test_le():
+    assert Chip(1) <= Chip(2)
+
+
+def test_le_eq():
+    assert Chip(2) <= Chip(2)
+
+
+def test_assert_eq():
+    with pytest.raises(InvalidOperationException):
+        Chip(1) == 1
+
+
+def test_assert_le():
+    with pytest.raises(InvalidOperationException):
+        Chip(1) <= 1
+
+
+def test_assert_ge():
+    with pytest.raises(InvalidOperationException):
+        Chip(1) >= 1
+
+
+def test_assert_add():
+    with pytest.raises(InvalidOperationException):
+        Chip(1) + 1
+
+
+def test_assert_sub():
+    with pytest.raises(InvalidOperationException):
+        Chip(1) - 1
+
+
+def test_add():
+    assert Chip(1) + Chip(1) == Chip(2)
+
+
+def test_sub():
+    assert Chip(2) - Chip(1) == Chip(1)
+
+
+def test_mul():
+    assert Chip(1) * 2 == Chip(2)
