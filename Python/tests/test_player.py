c41ee37a052478a9d65f7cdde736662ce6d1e3e5
Pavel Zamelin <pazamelingithub@yandex.ru>
2024-06-19 11:25:42 +0300

    Tests Use is instead of ==

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -3,38 +3,38 @@ from app import *
 def test_buy():
     player = Player()
     player.buy(Chip(10))
-    assert player._available_chips == Chip(10)
+    assert (player.has(Chip(10)) is True)
 
 def test_win():
     player = Player()
     player.buy(Chip(10))
     player.win(Chip(10))
-    assert player._available_chips == Chip(20)
+    assert (player.has(Chip(20)) is True)
 
 def test_take():
     player = Player()
     player.buy(Chip(10))
     player.take(Chip(5))
-    assert player._available_chips == Chip(5)
+    assert (player.has(Chip(5)) is True)
 
 def test_has_initial():
     player = Player()
-    assert player.has(Chip(0))
+    assert (player.has(Chip(0)) is True)
 
 def test_has_equals_chips():
     player = Player()
     player.buy(Chip(10))
-    assert player.has(Chip(10))
+    assert (player.has(Chip(10)) is True)
 
 def test_has_less_chips():
     player = Player()
     player.buy(Chip(10))
-    assert player.has(Chip(5))
+    assert (player.has(Chip(5)) is True)
 
 def test_has_not_enough_chips():
     player = Player()
     player.buy(Chip(5))
-    assert (player.has(Chip(10)) == False)
+    assert (player.has(Chip(10)) is False)
 
 
     
