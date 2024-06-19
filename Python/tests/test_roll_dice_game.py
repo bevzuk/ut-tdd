f3461c141746863a2c243cd2cb5ca7c481c4398c
just_sparta <vi.kuznetsov@yadro.com>
2024-06-19 15:04:22 +0300

    fake dice #3

--- Python/tests/test_roll_dice_game.py
+++ Python/tests/test_roll_dice_game.py
@@ -7,17 +7,27 @@ class FakeDice(IDice):
         return 5
 
 
-def test_plays_correctly():
+def test_player_wins():
     game = RollDiceGame(FakeDice())
     lucky_player = Player()
     lucky_player.join(game)
     lucky_player.buy(Chip(4))
 
-    game.bet(lucky_player, Bet(Chip(2), 5))
+    game.bet(lucky_player, Bet(Chip(4), 5))
     game.play()
 
-    assert lucky_player.has(Chip(14)), "Game plays wrong"
+    assert lucky_player.has(Chip(4) * 6), "Game plays wrong"
 
+def test_player_lose():
+    game = RollDiceGame(FakeDice())
+    lucky_player = Player()
+    lucky_player.join(game)
+    lucky_player.buy(Chip(4))
+
+    game.bet(lucky_player, Bet(Chip(4), 4))
+    game.play()
+
+    assert lucky_player.has(Chip(0)), "Game plays wrong"
 
 def test_plays_correctly_with_mock(mocker):
     mocked_dice = mocker.patch('Python.app.dice.Dice.roll', FakeDice)
