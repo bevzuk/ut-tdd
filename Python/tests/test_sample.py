cbdc396615711754287f6a22fd03735fbfe87a0a
Pavel Vorobyov <p.vorobyov@yadro.com>
2024-06-19 10:48:12 +0300

    Add structure for tests

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,5 +1,18 @@
 from app import *
+import unittest
 
+
+class TestBet(unittest.TestCase):
+    def test_bet_non_negative(self):
+        with self.assertRaises(Exception):
+            Bet(Chip(-2), 1)
+
+
+class TestGame(unittest.TestCase):
+    pass
+
+class TestPlayer(unittest.TestCase):
+    pass
 
 def test_pass():
     assert Chip(1) == Chip(1)
