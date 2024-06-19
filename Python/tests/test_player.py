416ce5d7f99c740dd4a109633de9d2094decf80a
Dmitry Kalyanov <kalyanowdima@mail.ru>
2024-06-19 16:42:21 +0300

    dsl

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -81,31 +81,6 @@ def test_when_not_enough_chips_cannot_bet(setup_player_with_5_chips_in_game):
     with pytest.raises(InvalidOperationException):
         game.bet(player, bet)
 
-# def test_can_win_with_gaining_chips(setup_player_with_5_chips_in_game):
-#     player, game = setup_player_with_5_chips_in_game
-#     dice = Dice()
-#     win_score = 1
-#     dice.roll = MagicMock(return_value = win_score)
-#     game.set_dice(dice)
-#     bet = Bet(Chip(5), win_score)
-#     game.bet(player, bet)
-
-#     game.play()
-
-#     assert player.has(Chip(5*6))
-
-def test_can_loose_with_loosing_chips(setup_player_with_5_chips_in_game):
-    player, game = setup_player_with_5_chips_in_game
-    dice = Dice()
-    win_score = 1
-    dice.roll = MagicMock(return_value = win_score)
-    bet = Bet(Chip(1), win_score * 2)
-    game.bet(player, bet)
-
-    game.play()
-
-    assert player.has(Chip(4))
-
 def test_can_win_with_gaining_chips_dsl():
     Create().player(is_cheater=True).buys_chips(n_chips=5).joins_game().bets_chips(n_chips=1).wins_game()
  
