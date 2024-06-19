e0acb69319b25733481abb6469e721baaaf324bf
Anna Grebneva <a.grebneva@yadro.com>
2024-06-19 12:58:11 +0300

    Ex3

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,52 +1,52 @@
 import unittest
 
+import pytest
+
 from app import *
 
 
 def test_pass():
     assert Chip(1) == Chip(1)
 
-class TestBet(unittest.TestCase):
-    def test_bet_creation(self):
-        chips = Chip(1)
-        assert Bet(chips, 1)
+def test_bet_creation():
+    chips = Chip(1)
 
-    def test_bet_should_not_create_with_float_score(self):
-        chips = Chip(1)
-        with self.assertRaises(BaseException):
-            Bet(chips, 1.0)
+    assert Bet(chips, 1)
 
-    def test_bet_should_not_create_with_string_score(self):
-        chips = Chip(1)
-        with self.assertRaises(BaseException):
-            Bet(chips, "one")
+def test_bet_should_not_create_with_float_score():
+    chips = Chip(1)
 
-class TestPlayer(unittest.TestCase):
-    def test_player_creation(self):
-        assert Player()
+    with pytest.raises(BaseException):
+        Bet(chips, 1.0)
 
-    def test_not_joined_player_should_not_be_in_game(self):
-        player = Player()
-        assert not player.is_in_game()
+def test_bet_should_not_create_with_string_score():
+    chips = Chip(1)
 
-    def test_joined_player_should_be_in_game(self):
-        player = Player()
-        game = RollDiceGame()
-        player.join(game)
-        assert player.is_in_game()
+    with pytest.raises(BaseException):
+        Bet(chips, "one")
 
-    def test_left_player_should_not_be_in_game(self):
-        player = Player()
-        game = RollDiceGame()
-        player.join(game)
-        player.leave_game()
-        assert not player.is_in_game()
+@pytest.fixture
+def set_game_and_joined_player():
+    player = Player()
+    game = RollDiceGame()
+    player.join(game)
+    return player, game
 
-    def test_player_can_buy_positive_chips(self):
-        player = Player()
-        assert player.buy(Chip(5))
+def test_player_creation():
+    assert Player()
 
-    def test_player_can_not_buy_negative_chips(self):
-        player = Player()
-        with self.assertRaises(BaseException):
-            player.buy(Chip(-5))
+def test_not_joined_player_should_not_be_in_game():
+    player = Player()
+
+    assert not player.is_in_game()
+
+def test_joined_player_should_be_in_game(set_game_and_joined_player):
+    player, game = set_game_and_joined_player
+    assert player.is_in_game()
+
+def test_left_player_should_not_be_in_game(set_game_and_joined_player):
+    player, game = set_game_and_joined_player
+
+    player.leave_game()
+
+    assert not player.is_in_game()
