07af824e450e3685e4eec062e061d7a4a11ff811
Aleksandr Korolev <a.korolev@yadro.com>
2024-06-19 10:49:08 +0300

    123

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,5 +1,13 @@
+
 from app import *
+import pytest
 
-
-def test_pass():
+def chip_eq():
     assert Chip(1) == Chip(1)
+
+def chip_eq():
+    with pytest.raises (InvalidOperationException()):
+        Chip(1) == 32
+
+def test_bet():
+    
