b04e3ab29d6c3e17386c7b2cae3f92c7a66e488b
p.yakovlev <p.yakovlev@yadro.com>
2024-06-19 10:58:35 +0300

    sumple tests

--- Python/tests/test_sample.py
+++ Python/tests/test_sample.py
@@ -1,5 +1,39 @@
 from app import *
+import pytest
+#from Python.app.Exceptions.invalid_operation_exception import InvalidOperationException
 
 
-def test_pass():
+def test_chip_eq():
     assert Chip(1) == Chip(1)
+
+# def test_chip_eq_badarg():
+#     with pytest.raises(Exception):
+#         Chip(1) == 31
+        
+def test_chip_ne():
+    assert Chip(1) != Chip(2)
+    
+    
+def test_chip_ge():
+    assert Chip(2) >= Chip(1)
+
+
+def test_chip_le():
+    assert Chip(1) <= Chip(2)
+    
+    
+def test_chip_sum():
+    assert Chip(1) + Chip(1) == Chip(2)
+    
+    
+def test_chip_sub():
+    assert Chip(2) - Chip(1) == Chip(1)
+    
+
+def test_chip_mul():
+    assert Chip(2) * 2 == Chip(4)
+    
+    
+    
+    
+    
