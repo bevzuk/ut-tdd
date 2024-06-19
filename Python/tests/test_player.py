f3dba10221feab10efc81b2ddfc11460c957f2e3
Ilya Naumov <i.n.1998@inbox.ru>
2024-06-19 11:54:06 +0300

    remove player and game class (duplicate player word in tests report)

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -1,59 +1,76 @@
 from app import *
 
 
-class TestsPlayer:
-    def test_can_join_game(self):
-        game = RollDiceGame()
-        player = Player()
+def test_can_join_game():
+    game = RollDiceGame()
+    player = Player()
 
-        player.join(game)
+    player.join(game)
 
-        assert player.is_in_game()
+    assert player.is_in_game()
 
-    def test_can_leave_game(self):
-        game = RollDiceGame()
-        player = Player()
-        player.join(game)
 
-        player.leave_game()
+def test_can_leave_game():
+    game = RollDiceGame()
+    player = Player()
+    player.join(game)
 
-        assert player.is_in_game() is False
+    player.leave_game()
 
-    def test_can_buy_chips(self):
-        player = Player()
+    assert player.is_in_game() is False
 
-        player.buy(Chip(10))
 
-        assert (player.has(Chip(10)) is True)
-        assert (player.has(Chip(11)) is False)
+def test_can_buy_chips():
+    player = Player()
 
-    def test_win(self):
-        player = Player()
-        player.buy(Chip(10))
-        player.win(Chip(10))
-        assert (player.has(Chip(20)) is True)
+    player.buy(Chip(10))
 
-    def test_take(self):
-        player = Player()
-        player.buy(Chip(10))
-        player.take(Chip(5))
-        assert (player.has(Chip(5)) is True)
+    assert (player.has(Chip(10)) is True)
+    assert (player.has(Chip(11)) is False)
 
-    def test_has_initial(self):
-        player = Player()
-        assert (player.has(Chip(0)) is True)
 
-    def test_has_equals_chips(self):
-        player = Player()
-        player.buy(Chip(10))
-        assert (player.has(Chip(10)) is True)
+def test_can_win():
+    player = Player()
+    player.buy(Chip(10))
 
-    def test_has_less_chips(self):
-        player = Player()
-        player.buy(Chip(10))
-        assert (player.has(Chip(5)) is True)
+    player.win(Chip(10))
 
-    def test_has_not_enough_chips(self):
-        player = Player()
-        player.buy(Chip(5))
-        assert (player.has(Chip(10)) is False)
+    assert (player.has(Chip(20)) is True)
+
+
+def test_can_take():
+    player = Player()
+    player.buy(Chip(10))
+
+    player.take(Chip(5))
+
+    assert (player.has(Chip(5)) is True)
+
+
+def test_has_initial_zero_chips():
+    player = Player()
+    assert (player.has(Chip(0)) is True)
+
+
+def test_has_equals_chips():
+    player = Player()
+
+    player.buy(Chip(10))
+
+    assert (player.has(Chip(10)) is True)
+
+
+def test_has_less_chips():
+    player = Player()
+
+    player.buy(Chip(10))
+
+    assert (player.has(Chip(5)) is True)
+
+
+def test_has_not_enough_chips():
+    player = Player()
+
+    player.buy(Chip(5))
+
+    assert (player.has(Chip(10)) is False)
